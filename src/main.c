f56aa0be041e2216a41ce753fb35402581ad0d68
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-12-05 17:20:12 -0300

    Implemented the GPIO_ReadFromInputPort function

--- src/main.c
+++ src/main.c
@@ -8,13 +8,19 @@
 #include "driver/gpio.h"
 
 
+GPIO_Handle_t pToGPIO1;
+
 int main() {
 
       // set clock to 16MHz
-      if (!initRCC()) return RCC_CFG_ERR;             //if clock is unable to be set to HSI, return RCC_CFG_ERR and end application
+      if (!initRCC()) return RCC_CFG_ERR;             //if clock is unable to be set to HSI, return RCC_CFG_ERR and terminate
 
 
       // init peripherals section
+
+      //GPIO
+      pToGPIO1.GPIO_PinConfig.GPIO_PinAltFunMode = GPIO_AFH_AF2;
+
 
 
       //main loop
