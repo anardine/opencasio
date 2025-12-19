7dc1829a4e2a9fe83351526f6bf78d3e1d3d4159
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-12-19 17:10:20 -0300

    Implemented I2C transmit

--- include/etc/error.h
+++ include/etc/error.h
@@ -6,7 +6,9 @@
 #define OPENCAS_ERROR_H
 
 enum ERROR_CODE {
-      RCC_CFG_ERR             = 1
+      RCC_CFG_ERR             = 1,
+//
+      I2C_CFG_ERR             = 6
 };
 
 #endif //OPENCAS_ERROR_H
--- src/driver/i2c.c
+++ src/driver/i2c.c
@@ -4,20 +4,14 @@
 
 #include "driver/i2c.h"
 
-void I2C_Init(I2C_Handle_t *pToI2CHandle) {
+uint8_t I2C_Init(I2C_Handle_t *pToI2CHandle) {
 
     if (pToI2CHandle->I2C_PinConfig.I2C_Mode == I2C_MODE_FMP) {
         //set Fast-Mode Plus if enabled
       (SYSCFG->cfgr1 |= (1 << 20));
     }
-      //enable the peripheral
-      pToI2CHandle->pI2Cx->cr1 |= (I2C_ENABLE << 0);
 
-}
-
-uint8_t I2C_Start (I2C_Handle_t *pToI2CHandle) {
-
-      uint8_t ackControl = pToI2CHandle->I2C_PinConfig.I2C_AckControl;
+           uint8_t ackControl = pToI2CHandle->I2C_PinConfig.I2C_AckControl;
       uint8_t freq = pToI2CHandle->I2C_PinConfig.I2C_ClockSpeed;
       uint8_t addr = pToI2CHandle->I2C_PinConfig.I2C_DeviceAddress;
       uint8_t dutyCycle = pToI2CHandle->I2C_PinConfig.I2C_DutyCycleForFastMode;
@@ -31,15 +25,15 @@ uint8_t I2C_Start (I2C_Handle_t *pToI2CHandle) {
     //TODO: Check the implementation details using the reference manual
 
     // Select I2C mode, adjust timing formula based on mode
-    if(freqMode == 0) {
+    if(freqMode == I2C_MODE_STANDARD) {
         // Standard Mode (max 100kHz), typical tr=1000ns tf=300ns
         t_rise_ns = 1000;
         t_fall_ns = 300;
-    } else if(freqMode == 1) {
+    } else if(freqMode == I2C_MODE_FM) {
         // Fast Mode (max 400kHz), typical tr=300ns tf=300ns
         t_rise_ns = 300;
         t_fall_ns = 300;
-    } else if (freqMode == 2){
+    } else if (freqMode == I2C_MODE_FMP){
         // Fast Mode Plus (max 1MHz), typical tr=120ns tf=120ns
         t_rise_ns = 120;
         t_fall_ns = 120;
@@ -90,29 +84,75 @@ uint8_t I2C_Start (I2C_Handle_t *pToI2CHandle) {
         // Apply timing configuration to I2C peripheral
         pToI2CHandle->pI2Cx->timingr = timingr;
 
+        //enable the peripheral
+        pToI2CHandle->pI2Cx->cr1 |= (I2C_ENABLE << 0);
+
         return 0; // configured!
     }
 
-    // If we reach here, no config is possible for the given arguments.
-    // You could assert(0), log an error, or fall back to a default config.
+    // Return 1 if not configured
+    return 1;
 
 }
 
 
 void I2C_DeInit(I2Cx_Reg_TypeDef *pI2Cx) {
+        //TODO: Implement de-init
+}
+
+
+uint8_t I2C_Transmit(I2C_Handle_t *pToI2CHandle, uint8_t *data, uint8_t length, uint8_t address) {
+
+    //put device in controller mode and configure sending bits
+    uint32_t volatile cr2 = 0;
+
+    // add the address to CR2
+    cr2 |= ((uint32_t)address << 1);
+
+    //enable transfer direction to be TX
+    cr2 &= ~(1 << 10);
+
+    //set address mode to 7bit addressing mode
+    cr2 &= ~(1 << 11);
+
+    //define the number of bytes to be sent
+    cr2 |= ((uint32_t)length << 16);
+
+    //define that a stop condition should be sent when the bytes are transmitted
+    cr2 |= (1 << 25);
+
+    //send start bit. To launch the communication, set the START bit of the I2C_CR2 register. The controller then automatically sends a START condition followed by the target address, either immediately if the BUSY flag is low, or tBUF time after the BUSY flag transits from high to lowstate. The BUSY flag is set upon sending the START condition.
+    cr2 |= (1 << 13);
+
+    //set cr2
+    pToI2CHandle->pI2Cx->cr2 = cr2;
+
+    //send data
+    for (int i = 0; i < length; i++) {
+        // Wait until TXIS (Transmit interrupt status) flag is set or NACK received
+        // TXIS is bit 1 in ISR
+        while (!(pToI2CHandle->pI2Cx->isr & (1 << 1)));
+
+        // Write data to TXDR
+        pToI2CHandle->pI2Cx->txdr = data[i];
+    }
+
+    // 4. Wait for the STOP condition to be detected
+    // STOPF is bit 5 in ISR
+    while (!(pToI2CHandle->pI2Cx->isr & (1 << 5)));
+
+    // Clear STOPF flag by writing to ICR (Interrupt Clear Register)
+    pToI2CHandle->pI2Cx->icr |= (1 << 5);
+
+    // Reset CR2 (optional, but good practice to clear NBYTES and ADDR)
+    pToI2CHandle->pI2Cx->cr2 = 0;
+
+    return 0;
 
 }
 
 
-void I2C_Transmit(I2C_Handle_t *pToI2CHandle, uint8_t *data, uint32_t length, uint8_t address) {
-
-
-
-
-}
-
-
-void I2C_Receive(I2C_Handle_t *pToI2CHandle, uint8_t *data, uint8_t address) {
+uint8_t I2C_Receive(I2C_Handle_t *pToI2CHandle, uint8_t *data, uint8_t address) {
 
 
 }
