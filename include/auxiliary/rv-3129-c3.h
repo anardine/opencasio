bbc091fdfe0c471684fe9d6b9989bb8640b13fc3
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-12-28 08:53:11 -0300

    Implemented the alarm init and clear functions

--- include/auxiliary/rv-3129-c3.h
+++ include/auxiliary/rv-3129-c3.h
@@ -9,6 +9,14 @@
 #include "driver/stm32wb55xx.h"
 #include "driver/i2c.h"
 
+
+//define Macros for settting and clearing bit
+#define ENABLE          0x01U
+#define DISABLE         0x00U
+
+//define the i2c addr of the RV-3129-C3
+uint8_t static const RTC_ADDR =  0xAC;
+
 uint8_t readFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t memAddr, uint8_t *data, uint8_t length);
 uint8_t writeToRTC(I2C_Handle_t *pToI2CHandle, uint8_t memAddr, uint8_t *data, uint8_t length);
 
--- src/auxiliary/rv-3129-c3.c
+++ src/auxiliary/rv-3129-c3.c
@@ -5,16 +5,12 @@
 
 #include "auxiliary/rv-3129-c3.h"
 
-#include <time.h>
-
 uint8_t readFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t memAddr, uint8_t *data, uint8_t length) {
 
-      //Any serial communication with the RV-3129-C3 starts with a “START condition” and terminates with the “STOP condition” No restart allowed on comms with the RV-3129. The RV-3129-C3 does not allow a repeated START. Therefore a STOP has to be released before the next START.
+      //Any serial communication with the RV-3129-C3 starts with a “START condition” and terminates with the “STOP condition” No restart allowed on comms with the RV-3129. The RV-3129-C3 does not allow a repeated START. Therefore a STOP has to be released before the next START
 
-      uint8_t rtcAddress =  0xAC; // defined at the RTC reference manual
-
-      I2C_Transmit(pToI2CHandle, data, memAddr, 0, rtcAddress);
-      I2C_Receive(pToI2CHandle, data, length, rtcAddress);
+      I2C_Transmit(pToI2CHandle, data, memAddr, 0, RTC_ADDR);
+      I2C_Receive(pToI2CHandle, data, length, RTC_ADDR);
 
       return 0;
 }
@@ -22,9 +18,7 @@ uint8_t readFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t memAddr, uint8_t *data, 
 
 uint8_t writeToRTC(I2C_Handle_t *pToI2CHandle, uint8_t memAddr, uint8_t *data, uint8_t length) {
 
-      uint8_t rtcAddress =  0xAC; // defined at the RTC reference manual
-
-      I2C_Transmit(pToI2CHandle, data, memAddr, length, rtcAddress);
+      I2C_Transmit(pToI2CHandle, data, memAddr, length, RTC_ADDR);
 
       return 0;
 }
@@ -34,6 +28,15 @@ uint8_t getTimeFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t *timePointer) {
       readFromRTC(pToI2CHandle,0x08, timePointer,1); //seconds
       readFromRTC(pToI2CHandle,0x09, (timePointer+1),1); //minutes
       readFromRTC(pToI2CHandle,0x0A, (timePointer+2),1); //hours
+
+      return 0;
+}
+
+uint8_t setTimeFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t *timePointer) {
+
+      writeToRTC(pToI2CHandle,0x08, timePointer,1); //seconds
+      writeToRTC(pToI2CHandle,0x09, (timePointer+1),1); //minutes
+      writeToRTC(pToI2CHandle,0x0A, (timePointer+2),1); //hours
 
       return 0;
 }
@@ -48,21 +51,38 @@ uint8_t getDateFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t *datePointer) {
       return 0;
 }
 
-uint8_t setTimeFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t *timePointer) {
-
-      writeToRTC(pToI2CHandle,0x08, timePointer,1); //seconds
-      writeToRTC(pToI2CHandle,0x09, (timePointer+1),1); //minutes
-      writeToRTC(pToI2CHandle,0x0A, (timePointer+2),1); //hours
-
-      return 0;
-}
-
 uint8_t setDateFromRTC(I2C_Handle_t *pToI2CHandle, uint8_t *datePointer) {
 
       writeToRTC(pToI2CHandle,0x0B, datePointer,1); //days
       writeToRTC(pToI2CHandle,0x0C, (datePointer+1),1); //weekday
       writeToRTC(pToI2CHandle,0x0D, (datePointer+2),1); //Months
       writeToRTC(pToI2CHandle,0x0E, (datePointer+3),1); //Years
+
+      return 0;
+}
+
+uint8_t alarmInit(I2C_Handle_t *pToI2CHandle) {
+
+      uint8_t flag = ENABLE;
+      uint8_t alarmSet;
+      //Enable the Alarm Interrupt in Control_INT.
+      writeToRTC(pToI2CHandle,0x01, &flag,1); //write to Control_INT 1 to AIE bit
+      readFromRTC(pToI2CHandle,0x01, &alarmSet,1); //read alarm value to check if alarm is enabled
+
+      return alarmSet; //return true if alarm is set init
+}
+
+uint8_t alarmClear(I2C_Handle_t *pToI2CHandle) {
+
+      uint8_t flag = DISABLE;
+
+      //Enable the Alarm Interrupt in Control_INT.
+      writeToRTC(pToI2CHandle,0x02, &flag,1); //write to Control_INT Flag the AF bit to clear Alarm INT
+
+      return 0;
+}
+
+uint8_t alarmSet(I2C_Handle_t *pToI2CHandle) {
 
       return 0;
 }
