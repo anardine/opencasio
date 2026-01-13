4fba31fd65d4219efee72ff5ab554059f2a09d99
Alessandro Nardinelli <anardine.ef@gmail.com>
2026-01-13 16:16:36 -0300

    Adjusted main to init I2C peripherals and arrays

--- src/main.c
+++ src/main.c
@@ -6,9 +6,15 @@
 #include "../include/etc/error.h"
 #include "driver/rcc.h"
 #include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "auxiliary/mmc5603nj.h"
+#include "auxiliary/rv-3129-c3.h"
 
 
 GPIO_Handle_t pToGPIO1;
+I2C_Handle_t pToI2C;
+magDataArray_t magRawDataArray; //array used to storage the results of the raw magnetic fields measurements
+magDataArray_t magHeadings; //array used to store the heading data ready to be served to the LCD
 
 int main() {
 
@@ -16,12 +22,15 @@ int main() {
       if (!initRCC()) return RCC_CFG_ERR;             //if clock is unable to be set to HSI, return RCC_CFG_ERR and terminate
 
 
-      // init peripherals section
+      // Init peripherals section
 
       //GPIOs
 
-      //main loop
+      //I2C
 
+
+
+      //main loop
       while (1)
       {
 
