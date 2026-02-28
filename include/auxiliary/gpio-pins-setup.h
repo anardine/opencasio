b27aa370ba6d0be3d9353370c7d8cdac211a44a3
Alessandro Nardinelli <anardine.ef@gmail.com>
2026-02-28 19:18:28 -0300

    adjusted the gpio init for using pointers as reference

--- include/auxiliary/gpio-pins-setup.h
+++ include/auxiliary/gpio-pins-setup.h
@@ -1,63 +1,103 @@
 #include "driver/gpio.h"
 
 // GPIOA handles
-GPIO_Handle_t pToGPIOA0;
-GPIO_Handle_t pToGPIOA1;
-GPIO_Handle_t pToGPIOA2;
-GPIO_Handle_t pToGPIOA3;
-GPIO_Handle_t pToGPIOA4;
-GPIO_Handle_t pToGPIOA5;
-GPIO_Handle_t pToGPIOA6;
-GPIO_Handle_t pToGPIOA7;
-GPIO_Handle_t pToGPIOA8;
-GPIO_Handle_t pToGPIOA9;
-GPIO_Handle_t pToGPIOA10;
-GPIO_Handle_t pToGPIOA11;
-GPIO_Handle_t pToGPIOA12;
-GPIO_Handle_t pToGPIOA13;
-GPIO_Handle_t pToGPIOA14;
-GPIO_Handle_t pToGPIOA15;
+GPIO_Handle_t *pToGPIOA0;
+GPIO_Handle_t *pToGPIOA1;
+GPIO_Handle_t *pToGPIOA2;
+GPIO_Handle_t *pToGPIOA3;
+GPIO_Handle_t *pToGPIOA4;
+GPIO_Handle_t *pToGPIOA5;
+GPIO_Handle_t *pToGPIOA6;
+GPIO_Handle_t *pToGPIOA7;
+GPIO_Handle_t *pToGPIOA8;
+GPIO_Handle_t *pToGPIOA9;
+GPIO_Handle_t *pToGPIOA10;
+GPIO_Handle_t *pToGPIOA11;
+GPIO_Handle_t *pToGPIOA12;
+GPIO_Handle_t *pToGPIOA13;
+GPIO_Handle_t *pToGPIOA14;
+GPIO_Handle_t *pToGPIOA15;
 
 // GPIOB handles
-GPIO_Handle_t pToGPIOB0;
-GPIO_Handle_t pToGPIOB1;
-GPIO_Handle_t pToGPIOB2;
-GPIO_Handle_t pToGPIOB3;
-GPIO_Handle_t pToGPIOB4;
-GPIO_Handle_t pToGPIOB5;
-GPIO_Handle_t pToGPIOB6;
-GPIO_Handle_t pToGPIOB7;
-GPIO_Handle_t pToGPIOB8;
-GPIO_Handle_t pToGPIOB9;
-GPIO_Handle_t pToGPIOB10;
-GPIO_Handle_t pToGPIOB11;
-GPIO_Handle_t pToGPIOB12;
-GPIO_Handle_t pToGPIOB13;
-GPIO_Handle_t pToGPIOB14;
-GPIO_Handle_t pToGPIOB15;
+GPIO_Handle_t *pToGPIOB0;
+GPIO_Handle_t *pToGPIOB1;
+GPIO_Handle_t *pToGPIOB2;
+GPIO_Handle_t *pToGPIOB3;
+GPIO_Handle_t *pToGPIOB4;
+GPIO_Handle_t *pToGPIOB5;
+GPIO_Handle_t *pToGPIOB6;
+GPIO_Handle_t *pToGPIOB7;
+GPIO_Handle_t *pToGPIOB8;
+GPIO_Handle_t *pToGPIOB9;
+GPIO_Handle_t *pToGPIOB10;
+GPIO_Handle_t *pToGPIOB11;
+GPIO_Handle_t *pToGPIOB12;
+GPIO_Handle_t *pToGPIOB13;
+GPIO_Handle_t *pToGPIOB14;
+GPIO_Handle_t *pToGPIOB15;
 
 // GPIOC handles
-GPIO_Handle_t pToGPIOC0;
-GPIO_Handle_t pToGPIOC1;
-GPIO_Handle_t pToGPIOC2;
-GPIO_Handle_t pToGPIOC3;
-GPIO_Handle_t pToGPIOC4;
-GPIO_Handle_t pToGPIOC5;
-GPIO_Handle_t pToGPIOC6;
-GPIO_Handle_t pToGPIOC7;
-GPIO_Handle_t pToGPIOC8;
-GPIO_Handle_t pToGPIOC9;
-GPIO_Handle_t pToGPIOC10;
-GPIO_Handle_t pToGPIOC11;
-GPIO_Handle_t pToGPIOC12;
-GPIO_Handle_t pToGPIOC13;
-GPIO_Handle_t pToGPIOC14;
-GPIO_Handle_t pToGPIOC15;
+GPIO_Handle_t *pToGPIOC0;
+GPIO_Handle_t *pToGPIOC1;
+GPIO_Handle_t *pToGPIOC2;
+GPIO_Handle_t *pToGPIOC3;
+GPIO_Handle_t *pToGPIOC4;
+GPIO_Handle_t *pToGPIOC5;
+GPIO_Handle_t *pToGPIOC6;
+GPIO_Handle_t *pToGPIOC7;
+GPIO_Handle_t *pToGPIOC8;
+GPIO_Handle_t *pToGPIOC9;
+GPIO_Handle_t *pToGPIOC10;
+GPIO_Handle_t *pToGPIOC11;
+GPIO_Handle_t *pToGPIOC12;
+GPIO_Handle_t *pToGPIOC13;
+GPIO_Handle_t *pToGPIOC14;
+GPIO_Handle_t *pToGPIOC15;
 
 // GPIOD handles
-GPIO_Handle_t pToGPIOD0;
-GPIO_Handle_t pToGPIOD1;
+GPIO_Handle_t *pToGPIOD0;
+GPIO_Handle_t *pToGPIOD1;
 
 // GPIOE handles
-GPIO_Handle_t pToGPIOE4;
+GPIO_Handle_t *pToGPIOE4;
 
+
+enum PeripheralCode {
+    LCD_PER,
+    I2C_PER,
+    BUZ_PER,
+    BTN_PER,
+};
+
+// Define a structure to hold pin configuration data
+typedef struct {
+    GPIOx_RegTypeDef *pGPIOx;
+    uint8_t pinNumber;
+    enum PeripheralCode perCode;
+} LCD_GPIO_PinDef_t;
+
+// Define all LCD pins with their GPIO port and pin number
+const LCD_GPIO_PinDef_t pinDefinitions[] = {
+    // GPIO A
+    {GPIOA, 1, LCD_PER},   {GPIOA, 2, LCD_PER},   {GPIOA, 3, LCD_PER},
+    {GPIOA, 4, LCD_PER},   {GPIOA, 5, BUZ_PER},{GPIOA, 6, LCD_PER},   {GPIOA, 7, LCD_PER},
+    {GPIOA, 8, LCD_PER},   {GPIOA, 9, LCD_PER},   {GPIOA, 10, LCD_PER},
+    {GPIOA, 15, LCD_PER},
+    // GPIO B
+    {GPIOB, 3, LCD_PER},   {GPIOB, 4, LCD_PER},   {GPIOB, 5, LCD_PER},
+    {GPIOB, 6, LCD_PER},   {GPIOB, 7, LCD_PER},   {GPIOB, 8, I2C_PER},
+    {GPIOB, 9, I2C_PER},   {GPIOB, 10, LCD_PER},   {GPIOB, 11, LCD_PER},
+    {GPIOB, 12, LCD_PER},
+    // GPIO C
+    {GPIOC, 0, LCD_PER},   {GPIOC, 1, LCD_PER},   {GPIOC, 2, LCD_PER},
+    {GPIOC, 3, BTN_PER},   {GPIOC, 4, LCD_PER},   {GPIOC, 5, LCD_PER},   {GPIOC, 6, LCD_PER},
+    {GPIOC, 10, LCD_PER},  {GPIOC, 11, LCD_PER},  {GPIOC, 12, LCD_PER},  {GPIOC, 13, BTN_PER},
+    // GPIO E
+    {GPIOE, 4, BTN_PER}
+};
+
+// TODO: Defensive programming with return of codes. For now the functions return void just for simplicity of first implementation
+void LCD_GPIO_Init(void);
+void I2C_GPIO_Init(void);
+void Buzzer_GPIO_Init(void);
+void Btn_GPIO_Init(void);
--- src/auxiliary/gpio-pins-setup.c
+++ src/auxiliary/gpio-pins-setup.c
@@ -1,42 +1,6 @@
 #include "driver/gpio.h"
 #include "auxiliary/gpio-pins-setup.h"
 
-//Define the ENUM
-
-enum PeripheralCode {
-    LCD_PER,
-    I2C_PER,
-    BUZ_PER,
-    BTN_PER,
-};
-
-// Define a structure to hold pin configuration data
-  typedef struct {
-      GPIOx_RegTypeDef *pGPIOx;
-      uint8_t pinNumber;
-      enum PeripheralCode perCode;
-  } LCD_GPIO_PinDef_t;
-
-// Define all LCD pins with their GPIO port and pin number
-const LCD_GPIO_PinDef_t pinDefinitions[] = {
-    // GPIO A
-    {GPIOA, 1, LCD_PER},   {GPIOA, 2, LCD_PER},   {GPIOA, 3, LCD_PER},
-    {GPIOA, 4, LCD_PER},   {GPIOA, 5, BUZ_PER},{GPIOA, 6, LCD_PER},   {GPIOA, 7, LCD_PER},
-    {GPIOA, 8, LCD_PER},   {GPIOA, 9, LCD_PER},   {GPIOA, 10, LCD_PER},
-    {GPIOA, 15, LCD_PER},
-    // GPIO B
-    {GPIOB, 3, LCD_PER},   {GPIOB, 4, LCD_PER},   {GPIOB, 5, LCD_PER},
-    {GPIOB, 6, LCD_PER},   {GPIOB, 7, LCD_PER},   {GPIOB, 8, I2C_PER},
-    {GPIOB, 9, I2C_PER},   {GPIOB, 10, LCD_PER},   {GPIOB, 11, LCD_PER},
-    {GPIOB, 12, LCD_PER},
-    // GPIO C
-    {GPIOC, 0, LCD_PER},   {GPIOC, 1, LCD_PER},   {GPIOC, 2, LCD_PER},
-    {GPIOC, 3, BTN_PER},   {GPIOC, 4, LCD_PER},   {GPIOC, 5, LCD_PER},   {GPIOC, 6, LCD_PER},
-    {GPIOC, 10, LCD_PER},  {GPIOC, 11, LCD_PER},  {GPIOC, 12, LCD_PER},  {GPIOC, 13, BTN_PER},
-    // GPIO E
-    {GPIOE, 4, BTN_PER}
-};
-
 //setup the characteristics for the GPIO setup for all the LCD pins
 void LCD_GPIO_Init(void) {
 
@@ -46,9 +10,9 @@ void LCD_GPIO_Init(void) {
 
     // Array of handles for GPIO pins
     GPIO_Handle_t *pins[] = {
-        &pToGPIOA1,  &pToGPIOA2,  &pToGPIOA3,  &pToGPIOA4,  &pToGPIOA6,  &pToGPIOA7, &pToGPIOA8,  &pToGPIOA9,  &pToGPIOA10,  &pToGPIOA15, //A
-        &pToGPIOB3,  &pToGPIOB4,  &pToGPIOB5,  &pToGPIOB6,  &pToGPIOB7, &pToGPIOB10, &pToGPIOB11, &pToGPIOB12, //B
-        &pToGPIOC0,  &pToGPIOC1,  &pToGPIOC2,  &pToGPIOC4,  &pToGPIOC5,  &pToGPIOC6,  &pToGPIOC10, &pToGPIOC11, &pToGPIOC12, //C
+        pToGPIOA1,  pToGPIOA2,  pToGPIOA3,  pToGPIOA4,  pToGPIOA6,  pToGPIOA7, pToGPIOA8,  pToGPIOA9,  pToGPIOA10,  pToGPIOA15, //A
+        pToGPIOB3,  pToGPIOB4,  pToGPIOB5,  pToGPIOB6,  pToGPIOB7, pToGPIOB10, pToGPIOB11, pToGPIOB12, //B
+        pToGPIOC0,  pToGPIOC1,  pToGPIOC2,  pToGPIOC4,  pToGPIOC5,  pToGPIOC6,  pToGPIOC10, pToGPIOC11, pToGPIOC12, //C
     };
 
     // Single loop initialization
@@ -65,7 +29,7 @@ void I2C_GPIO_Init(void) {
     GPIO_PinConfig_t defaultCfg;
 
     GPIO_Handle_t *pins[] = {
-        &pToGPIOB8, &pToGPIOB9
+        pToGPIOB8, pToGPIOB9
     };
 
     defaultCfg.GPIO_PinMode = GPIO_MODE_AF;
@@ -85,33 +49,27 @@ void I2C_GPIO_Init(void) {
 
 void Buzzer_GPIO_Init(void) {
 
-    GPIO_Handle_t pinPA5 = &pToGPIOA5;
+    pToGPIOA5->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUTPUT;
+    pToGPIOA5->GPIO_PinConfig.GPIO_PinNumber = 5;
+    pToGPIOA5->pGPIOx = GPIOA;
 
-    pinPA5.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUTPUT;
-    pinPA5.GPIO_PinConfig.GPIO_PinNumber = 5;
-    pinPA5->pGPIOx = GPIOA;
-
-    GPIO_Init(pinPA5);
+    GPIO_Init(pToGPIOA5);
 
 }
 
 void Btn_GPIO_Init(void) {
 
-    GPIO_Handle_t pinPC13 = &pToGPIOC13;    // BTN LED
-    GPIO_Handle_t pinPC3 = &pToGPIOC3;      // BTN MODE
-    GPIO_Handle_t pinPE4 = &pToGPIOE4;    // BTN ALARM
+    pToGPIOC13->pGPIOx = GPIOC;
+    pToGPIOC13->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
+    GPIO_Init(pToGPIOC13);
 
-    pinPC13->pGPIOx = GPIOC;
-    pinPC13.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
-    GPIO_Init(pinPC13);
+    pToGPIOC3->pGPIOx = GPIOC;
+    pToGPIOC3->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
+    GPIO_Init(pToGPIOC3);
 
-    pinPC3->pGPIOx = GPIOC;
-    pinPC3.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
-    GPIO_Init(pinPC3);
-
-    pinPE4->pGPIOx = GPIOE;
-    pinPE4.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
-    GPIO_Init(pinPE4);
+    pToGPIOE4->pGPIOx = GPIOE;
+    pToGPIOE4->GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_INPUT;
+    GPIO_Init(pToGPIOE4);
 
 }
 
